8545800eb94cdb204899028608b6fff2a6dabc5c
Nikita <nkt.ochkin@gmail.com>
2024-07-13 02:33:15 +0300

    Initial commit

---
+++ VisageVerify/VisageVerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisageVerifyApp.swift
+//  VisageVerify
+//
+//  Created by Nikita Ochkin on 13.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct VisageVerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
