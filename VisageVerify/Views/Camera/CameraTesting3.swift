87e9dc4261bb8cb6013852401d48e735f1be7ac9
Nikita <nkt.ochkin@gmail.com>
2024-08-28 03:17:45 +0300

    completely redesigned camera registration screen, added a button to turn on chaos state

---
+++ VisageVerify/Views/Camera/CameraTesting3.swift
@@ -0,0 +1,18 @@
+//
+//  CameraTesting3.swift
+//  VisageVerify
+//
+//  Created by Nikita Ochkin on 27.08.2024.
+//
+
+import SwiftUI
+
+struct CameraTesting3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraTesting3()
+}
---
+++ VisageVerify/Views/Testing/Testing3.swift
@@ -0,0 +1,18 @@
+//
+//  Testing3.swift
+//  VisageVerify
+//
+//  Created by Nikita Ochkin on 28.08.2024.
+//
+
+import SwiftUI
+
+struct Testing3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Testing3()
+}
